78c88e19efab3f13a7d6adf4b8c8d62f477903a4
laasyamanikonda <121774687+laasyamanikonda@users.noreply.github.com>
2025-02-01 21:40:28 -0600

    idk

--- click.js
+++ click.js
@@ -2,14 +2,12 @@ let images = document.querySelectorAll(".img");
 
 images.forEach(function(img) {
     img.addEventListener("mousedown", function() {
-      img.style.width = "150px"; // Shrink the image
+      img.style.width = "175px"; // Shrink the image
     });
   
     img.addEventListener("mouseup", function() {
       img.style.width = "200px"; // Restore to original size
     });
   
-    img.addEventListener("mouseleave", function() {
-      img.style.width = "200px"; // Restore to original size
-    });
+
   });
